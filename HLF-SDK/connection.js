f71a7bb6f2fd60e5b18e785af7a9cb76add942c1
GiHoon <rlgns98kr@gmail.com>
2020-05-28 20:44:30 +0900

    test

--- HLF-SDK/connection.js
+++ HLF-SDK/connection.js
@@ -3,34 +3,42 @@ const path = require('path');
 const FabricCAServices = require('fabric-ca-client');
 const { FileSystemWallet, X509WalletMixin, Gateway } = require('fabric-network');
 
-
-
 enroll = async () => {
     const ccpPath = path.resolve(__dirname, 'connection_config.json');
     //ccp란 common connection profile의 약자
     const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
     const ccp = JSON.parse(ccpJSON);
     // 인증기관과 통신할 수 있는 객체 생성
-    const caURL = ccp.certificateAuthorities['ca.example.com'].url;
-    const ca = new FabricCAServices(caURL);
 
     try {
-        // 신원 증명서를 저장할 지갑 생성
+        // Create a new CA client for interacting with the CA.
+        const caInfo = ccp.certificateAuthorities['ca.example.com'];
+        const caTLSCACerts = caInfo.tlsCACerts.pem;
+        const ca = new FabricCAServices(caInfo.url, { trustedRoots: caTLSCACerts, verify: false }, caInfo.caName);
+
+        // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
         const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
 
-        // admin신원 증명서가 있는지 확인
+        // Check to see if we've already enrolled the admin user.
         const adminExists = await wallet.exists('admin');
-        if (!adminExists) {
-            // Enroll the admin user, and import the new identity into the wallet.
-            const enrollment = await ca.enroll({ enrollmentID: 'admin', enrollmentSecret: 'adminpw' });
-            const identity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
-            await wallet.import('admin', identity);
-            console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
+        if (adminExists) {
+            console.log('An identity for the admin user "admin" already exists in the wallet');
+            return;
         }
-    } catch (e) {
-        console.log(e);
+
+        // Enroll the admin user, and import the new identity into the wallet.
+        const enrollment = await ca.enroll({ enrollmentID: 'admin', enrollmentSecret: 'adminpw' });
+        const identity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
+        await wallet.import('admin', identity);
+        console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
+
+    } catch (error) {
+        console.error(`Failed to enroll admin user "admin": ${error}`);
+        process.exit(1);
     }
+
     try {
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
